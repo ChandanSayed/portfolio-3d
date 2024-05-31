0f6d322d2b8aec0045107ef784811742177989c2
ChandanSayed <hangzhou635@gmail.com>
2024-05-31 11:59:49 +0600

    About Section added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import React from "react";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+import { Tilt } from "react-tilt";
+
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className="xs:w-[250px] w-full">
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450
+        }}
+        className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+      >
+        <img src={icon} alt="web-development" className="w-16 h-16 object-contain" />
+
+        <h3 className="text-white text-[20px] font-bold text-center">{title}</h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
 
-export default About
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        I'm a seasoned software developer with extensive experience in TypeScript and JavaScript,
+        and deep expertise in frameworks like React, Node.js, and Three.js. I pride myself on being
+        a fast learner and thrive in collaborative environments, working closely with clients to
+        design and implement efficient, scalable, and user-friendly solutions. Whether you're
+        looking to bring innovative ideas to life or solve complex real-world problems, let's team
+        up to make it happen!
+      </motion.p>
+
+      <div className="mt-20 grid md:grid-cols-2 lg:grid-cols-4 gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
